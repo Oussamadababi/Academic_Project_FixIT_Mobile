56e603227bac8118ed25b194cabd69a69456d499
Asus <Asus@DESKTOP-1MB1J2L>
2019-12-27 01:14:32 +0100

    aa

--- src/com/codename1/uikit/cleanmodern/OffreAjout.java
+++ src/com/codename1/uikit/cleanmodern/OffreAjout.java
@@ -5,10 +5,12 @@
  */
 package com.codename1.uikit.cleanmodern;
 
-import Entites.Echange;
-import Entites.User;
-import Service.Posteur_service;
+
+import Entites.Categorie;
+import Entites.Offre;
+import Entites.Task;
 import Service.Session;
+import com.codename1.components.ImageViewer;
 import com.codename1.components.ScaleImageLabel;
 import com.codename1.components.SpanLabel;
 import com.codename1.components.ToastBar;
@@ -19,6 +21,7 @@ import com.codename1.io.NetworkEvent;
 import com.codename1.io.NetworkManager;
 import com.codename1.ui.Button;
 import com.codename1.ui.ButtonGroup;
+import com.codename1.ui.ComboBox;
 import com.codename1.ui.Component;
 import static com.codename1.ui.Component.BOTTOM;
 import static com.codename1.ui.Component.CENTER;
@@ -26,15 +29,17 @@ import static com.codename1.ui.Component.LEFT;
 import static com.codename1.ui.Component.RIGHT;
 import com.codename1.ui.Container;
 import com.codename1.ui.Display;
+import com.codename1.ui.EncodedImage;
 import com.codename1.ui.FontImage;
-import com.codename1.ui.Form;
 import com.codename1.ui.Graphics;
 import com.codename1.ui.Image;
 import com.codename1.ui.Label;
 import com.codename1.ui.RadioButton;
 import com.codename1.ui.Tabs;
 import com.codename1.ui.TextArea;
+import com.codename1.ui.TextField;
 import com.codename1.ui.Toolbar;
+import com.codename1.ui.URLImage;
 import com.codename1.ui.events.ActionEvent;
 import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BorderLayout;
@@ -43,58 +48,34 @@ import com.codename1.ui.layouts.FlowLayout;
 import com.codename1.ui.layouts.GridLayout;
 import com.codename1.ui.layouts.LayeredLayout;
 import com.codename1.ui.plaf.Style;
-import com.codename1.ui.plaf.UIManager;
+import com.codename1.ui.spinner.Picker;
 import com.codename1.ui.util.Resources;
-import com.codename1.ui.util.UIBuilder;
+import static com.codename1.uikit.cleanmodern.affiche_jobeur.idj;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import javafx.scene.control.DatePicker;
 
 /**
  *
- * @author Asus
+ * @author waelb
  */
-public class affiche_jobeur extends BaseForm{
-      static int idj;
-   
-    public affiche_jobeur(Resources res) {
-        
-//         Form f ;
-//        Button p;
-//       Label label_nom;
-//         label_nom = new Label();
-//          f = new Form();
-//      f.add(label_nom);
-//     
-//      p= new Button("bachoul");
-//      f.add(p);
-//      label_nom.setText("aaasba");
-//        Posteur_service  a = new Posteur_service();
-//       User b=new User();
-//       ArrayList<User> listJobeurs = new ArrayList<>();
-//           listJobeurs=a.affich_jobeur("Electriciter");
-//            b= listJobeurs.get(0);
-//            System.out.println(b);
-//            
-//            for(User u:listJobeurs)
-//            {
-//             
-//               
-//                
-//     
-//        //To change body of generated methods, choose Tools | Templates.
-//    }
-      
-        
-         super("Jobeurs Electriciter", BoxLayout.y());
+public class OffreAjout extends BaseForm {
+    Image imgg;
+        EncodedImage enc ;
+    
+    
+    public OffreAjout(Resources res) {
+       
+        super("Remplir le formulaire", BoxLayout.y());
+        ImageViewer img= new ImageViewer();
         Toolbar tb = new Toolbar(true);
         setToolbar(tb);
         getTitleArea().setUIID("Container");
-        setTitle("Jobeurs Electriciter");
+        setTitle("Offre");
         getContentPane().setScrollVisible(false);
-        
         
         super.addSideMenu(res);
         tb.addSearchCommand(e -> {});
@@ -103,8 +84,8 @@ public class affiche_jobeur extends BaseForm{
 
         Label spacer1 = new Label();
         Label spacer2 = new Label();
-        addTab(swipe, res.getImage("electricte.JPG"), spacer1, "", "", " ");
-        
+        addTab(swipe, res.getImage("home1.jpg"), spacer1, "16 Likes  ", "85 Comments", "Integer ut placerat purued non dignissim neque. ");
+        addTab(swipe, res.getImage("home2.jpg"), spacer2, "100 Likes  ", "66 Comments", "Dogs are cute: story at 11");
                 
         swipe.setUIID("Container");
         swipe.getContentPane().setUIID("Container");
@@ -114,13 +95,13 @@ public class affiche_jobeur extends BaseForm{
         int size = Display.getInstance().convertToPixels(1);
         Image unselectedWalkthru = Image.createImage(size, size, 0);
         Graphics g = unselectedWalkthru.getGraphics();
-        g.setColor(0xffffff);
+        g.setColor(0xF4BE1B);
         g.setAlpha(100);
         g.setAntiAliased(true);
         g.fillArc(0, 0, size, size, 0, 360);
         Image selectedWalkthru = Image.createImage(size, size, 0);
         g = selectedWalkthru.getGraphics();
-        g.setColor(0xffffff);
+        g.setColor(0xF4BE1B);
         g.setAntiAliased(true);
         g.fillArc(0, 0, size, size, 0, 360);
         RadioButton[] rbs = new RadioButton[swipe.getTabCount()];
@@ -145,53 +126,101 @@ public class affiche_jobeur extends BaseForm{
         add(LayeredLayout.encloseIn(swipe, radioContainer));
         
         ButtonGroup barGroup = new ButtonGroup();
-        RadioButton all = RadioButton.createToggle("Liste Jobeurs", barGroup);
+        RadioButton all = RadioButton.createToggle("All", barGroup);
         all.setUIID("SelectBar");
-        RadioButton featured = RadioButton.createToggle("Mes offres ", barGroup);
+                all.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+               new Produit(res).show();
+            }
+        });
+       // all.setSelectedStyle(style);
+        RadioButton featured = RadioButton.createToggle("My products", barGroup);
         featured.setUIID("SelectBar");
-        
-           featured .addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent evt) {
-                        
-                        {
-                       
-                           new MesEchangeForm (res).show();
-                          
-                        }
-                        }
-                });
-     
+        featured.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+               new MesProduits(res).show();
+            }
+        });
+        RadioButton popular = RadioButton.createToggle("Add products", barGroup);
+        popular.setUIID("SelectBar");
+
         Label arrow = new Label(res.getImage("news-tab-down-arrow.png"), "Container");
         
         add(LayeredLayout.encloseIn(
-                GridLayout.encloseIn(2, all, featured),
+                GridLayout.encloseIn(3, all, featured, popular),
                 FlowLayout.encloseBottom(arrow)
         ));
-        
         
         all.setSelected(true);
         arrow.setVisible(false);
         addShowListener(e -> {
             arrow.setVisible(true);
-            updateArrowPosition(all, arrow);
+            updateArrowPosition(popular, arrow);
         });
         bindButtonSelection(all, arrow);
         bindButtonSelection(featured, arrow);
-        
-      
+        bindButtonSelection(popular, arrow);
+       // bindButtonSelection(myFavorite, arrow);
         
         // special case for rotation
         addOrientationListener(e -> {
             updateArrowPosition(barGroup.getRadioButton(barGroup.getSelectedIndex()), arrow);
-            
         });
-     int id=Session.getInstance().getLoggedInUser().getId();
-        
+       
+        TextField adress = new TextField();
+        adress.setUIID("TextFieldBlack");
+        addStringValue("adress",adress);
+        Picker date = new Picker();
+         addStringValue("date",date);
+        int tel8=Session.getInstance().getLoggedInUser().getTel();
+        String tel2=Integer.toString(tel8);
+        TextField Num = new TextField(tel2);
+        Num.setUIID("TextFieldBlack");
+        addStringValue("Portable",Num);
+        TextArea Description = new TextArea();
+        Description.setUIID("TextAreaBlack");
+        addStringValue("Description",Description);
+        Button ajouter1=new Button("Ajouter");
+        addStringValue("",ajouter1);
+        int idd= Session.getInstance().getLoggedInUser().getId();
+        int num=Integer.parseInt(Num.getText());
         ConnectionRequest con = new ConnectionRequest();
-         con.setUrl("http://localhost/fixitweb1/web/app_dev.php/oussama/affichemobile/Electriciter"); 
-      ArrayList<User> listJobeurs = new ArrayList<>();
-            con.addResponseListener((NetworkEvent evt) -> {
+  
+       
+        ajouter1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                System.out.println(idj);
+                int num=Integer.parseInt(Num.getText());
+                Offre of= new Offre(adress.getText(),num,Description.getText(),idd);
+                ConnectionRequest con = new ConnectionRequest();// création d'une nouvelle demande de connexion
+        String Url = "http://localhost/fixitweb1/web/app_dev.php/oussama/ajoutoffre?adress="+of.getAdress()+"&description_offre="+of.getDesciption()+"&tel="+of.getTel()+"&idposteurFg="+of.getIdpo();
+        con.setUrl(Url);// Insertion de l'URL de notre demande de connexion
+
+        con.addResponseListener((e) -> {
+            String str = new String(con.getResponseData());//Récupération de la réponse du serveur
+            System.out.println(str);//Affichage de la réponse serveur sur la console
+
+        });
+        NetworkManager.getInstance().addToQueueAndWait(con);
+                
+                
+            }
+        
+        
+        
+        
+        
+                
+                
+                
+      /*  int idd= Session.getInstance().getLoggedInUser().getId();
+        ConnectionRequest con = new ConnectionRequest();
+        con.setUrl("http://localhost/fixitweb1/web/app_dev.php/wael/afficherMesproduitMobile/"+idd);  
+        con.addResponseListener((NetworkEvent evt) -> {
+            ArrayList<Task> listTasks = new ArrayList<>();
             try {
                 //(new String(con.getResponseData()));
                 String aff=new String(con.getResponseData());
@@ -199,34 +228,50 @@ public class affiche_jobeur extends BaseForm{
                 Map<String, Object> tasks = j.parseJSON(new CharArrayReader(aff.toCharArray()));
                 List<Map<String, Object>> list = (List<Map<String, Object>>) tasks.get("root");
              for (Map<String, Object> obj : list) {
-                User U = new User();
-           addButton3(res.getImage("electricte.JPG"),false,55,55,obj.get("nom").toString(),obj.get("prenom").toString(),obj.get("tel").toString(),obj.get("specialite").toString(),res);
-                 Button b =new Button("commander");
-                 addStringValue("",b);
-                 b.addActionListener(new ActionListener() {
-                @Override
-                    public void actionPerformed(ActionEvent evt) {
-                       new OffreAjout(res).show();
-                       float id1 = Float.parseFloat(obj.get("id").toString());
-                        idj=(int)id1;
-                       
-                    }
-                });
-                 
-                listJobeurs.add(U);
+                Task e = new Task();
+                float id = Float.parseFloat(obj.get("id").toString());
+                float prix = Float.parseFloat(obj.get("prix").toString());
+                float num = Float.parseFloat(obj.get("num").toString());
+                e.setId((int) id);
+               int numero=(int) num-1;
+               int prixx=(int) prix;
+               String description= obj.get("description").toString();
+               String nomproduit= obj.get("nomproduit").toString();
+
+ 
+        try {
+            enc = EncodedImage.create("/load.png");
+        } catch (IOException ex) { 
+        }
+ 
+        if (obj.get("imageProduit")==null)
+         {
+             String url2="http://localhost/fixitweb1/web/upload/aucune.jpg";
+             imgg=URLImage.createToStorage(enc,url2,url2,URLImage.RESIZE_SCALE);
+         }
+         else{
+            String url="http://localhost/fixitweb1/web/upload/"+obj.get("imageProduit").toString();
+             imgg=URLImage.createToStorage(enc,url,url,URLImage.RESIZE_SCALE);
+         }
+        
+        
+        addButton(imgg, nomproduit, false, 26, 32, description,prixx,numero);
+
+           LinkedHashMap<String,Object> obj1 =  (LinkedHashMap<String,Object>) obj.get("idposteurFg") ;
+           int pos = 1;
+           e.setUsername(obj1.get("username").toString());
+                listTasks.add(e);
             }} 
             catch (IOException ex) {
             }
-                });
-              
+        });
         NetworkManager.getInstance().addToQueueAndWait(con);
-       // addButton(res.getImage("news-item-1.jpg"), "Morbi per tincidunt tellus sit of amet eros laoreet.", false, 26, 32);
-       // addButton(res.getImage("news-item-2.jpg"), "Fusce ornare cursus masspretium tortor integer placera.", true, 15, 21);
-        //addButton(res.getImage("news-item-3.jpg"), "Maecenas eu risus blanscelerisque massa non amcorpe.", false, 36, 15);
-        //addButton(res.getImage("news-item-4.jpg"), "Pellentesque non lorem diam. Proin at ex sollicia.", false, 11, 9);
-      
-    }
-     
+       */
+    });}
+    
+   
+    
+    
     
     private void updateArrowPosition(Button b, Label arrow) {
         arrow.getUnselectedStyle().setMargin(LEFT, b.getX() + b.getWidth() / 2 - arrow.getWidth() / 2);
@@ -272,46 +317,8 @@ public class affiche_jobeur extends BaseForm{
 
         swipe.addTab("", page1);
     }
-
-private void addButton3(Image img, boolean liked, int likeCount, int commentCount,String Proff,String souh,String description,String nom,Resources res) {
-       int height = Display.getInstance().convertToPixels(11.5f);
-       int width = Display.getInstance().convertToPixels(14f);
-       Button image = new Button(img.fill(width, height));
-       image.setUIID("Label");
-       Container cnt = BorderLayout.west(image);
-       cnt.setLeadComponent(image);
-      
-     
-
-       Label likes = new Label(likeCount + " Likes  ", "NewsBottomLine");
-      // Label likes1 = new Label("asazd" + " Likes  ", "NewsBottomLine");
-       likes.setTextPosition(RIGHT);
-
-      
-      
-       Label num = new Label("Prenom:  "+Proff , "NewsBottomLine"); 
-      // FontImage.setMaterialIcon(num, FontImage.MATERIAL_PHONE);
-      
-       Label likes1 = new Label("Nom  :  " + souh  , "NewsBottomLine");
-      // FontImage.setMaterialIcon(likes1, FontImage.MATERIAL_PAYMENT);
-        
-       Label comments = new Label( " Tel : "+description , "NewsBottomLine"); 
-     //  FontImage.setMaterialIcon(comments, FontImage.MATERIAL_CHAT);
-         Label user = new Label( "Spécialite  : "+nom , "NewsBottomLine"); 
-       cnt.add(BorderLayout.CENTER, 
-               BoxLayout.encloseY(
-                      
-                       BoxLayout.encloseX(likes1,num ),
-                       comments,user
-                       
-               ));
- 
-       add(cnt);
-         
-     
-   }
     
-   private void addButton(Image img, String title, boolean liked, int likeCount, int commentCount) {
+   private void addButton(Image img, String title, boolean liked, int likeCount, int commentCount,String description,int Prix,int Num) {
        int height = Display.getInstance().convertToPixels(11.5f);
        int width = Display.getInstance().convertToPixels(14f);
        Button image = new Button(img.fill(width, height));
@@ -323,24 +330,35 @@ private void addButton3(Image img, boolean liked, int likeCount, int commentCoun
        ta.setEditable(false);
 
        Label likes = new Label(likeCount + " Likes  ", "NewsBottomLine");
+      // Label likes1 = new Label("asazd" + " Likes  ", "NewsBottomLine");
        likes.setTextPosition(RIGHT);
-       if(!liked) {
+      /* if(!liked) {
            FontImage.setMaterialIcon(likes, FontImage.MATERIAL_FAVORITE);
        } else {
            Style s = new Style(likes.getUnselectedStyle());
-           s.setFgColor(0xff2d55);
+           s.setFgColor(0xF4BE1B);
            FontImage heartImage = FontImage.createMaterial(FontImage.MATERIAL_FAVORITE, s);
            likes.setIcon(heartImage);
-       }
-       Label comments = new Label(commentCount + " Comments", "NewsBottomLine");
-       FontImage.setMaterialIcon(likes, FontImage.MATERIAL_CHAT);
-       
+       }*/
+      
+      
+       Label num = new Label(" Phone: "+ Num , "NewsBottomLine"); 
+       FontImage.setMaterialIcon(num, FontImage.MATERIAL_PHONE);
+      
+       Label likes1 = new Label("Prix:  " + Prix +"  $" , "NewsBottomLine");
+       FontImage.setMaterialIcon(likes1, FontImage.MATERIAL_PAYMENT);
+        
+       Label comments = new Label( " Description : "+description , "NewsBottomLine"); 
+       FontImage.setMaterialIcon(comments, FontImage.MATERIAL_CHAT);
        
        cnt.add(BorderLayout.CENTER, 
                BoxLayout.encloseY(
                        ta,
-                       BoxLayout.encloseX(likes, comments)
+                       BoxLayout.encloseX(likes1,num ),
+                       comments
+                       
                ));
+ 
        add(cnt);
        image.addActionListener(e -> ToastBar.showMessage(title, FontImage.MATERIAL_INFO));
    }
@@ -352,15 +370,11 @@ private void addButton3(Image img, boolean liked, int likeCount, int commentCoun
             }
         });
     }
-    
-       private void addStringValue(String s, Component v) {
+
+     private void addStringValue(String s, Component v) {
         add(BorderLayout.west(new Label(s, "PaddedLabel")).
                 add(BorderLayout.CENTER, v));
         add(createLineSeparator(0xF4BE1B));
         
     }
 }
-            
-    
-    
-
